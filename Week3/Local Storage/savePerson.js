4af9b0ba3ca512b6f6f17ff64a53c169288777b5
John Pwajok <johnpwajok97@hotmail.com>
2018-02-26 11:48:38 +0200

    Added LocalStorage

--- Week3/Local Storage/savePerson.js
+++ Week3/Local Storage/savePerson.js
@@ -19,3 +19,28 @@ function display(nameInput){
 window.addEventListener("load", doFirst, false);
 
 
+/*const input = document.getElementById('nameInput');
+let itemsArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
+
+localStorage.setItem('items', JSON.stringify(itemsArray));
+const data = JSON.parse(localStorage.getItem('items'));
+
+const liMaker = (text) => {
+  const li = document.createElement('li');
+  li.textContent = text;
+  ul.appendChild(li);
+}
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  itemsArray.push(input.value);
+  localStorage.setItem('items', JSON.stringify(itemsArray));
+  liMaker(input.value);
+  input.value = "";
+});
+*/
+data.forEach(item => {
+  liMaker(item);
+});
+
